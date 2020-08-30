bce30bcba39725179b561580ae83c4eddd90b826
vthoppae <venkat.thoppae@gmail.com>
2020-08-30 16:29:22 -0400

    removing learn files

--- learn/recipes-app/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Menu from './components/Menu';
-import data from './data/recipes.json';
-
-render(<Menu recipes={data} />, document.getElementById('root'));
