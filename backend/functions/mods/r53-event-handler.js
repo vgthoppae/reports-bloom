1391624b6647d2e793889b6d125373662360e5f9
vthoppae <venkat.thoppae@gmail.com>
2020-11-21 17:01:37 -0500

    ssm changes

--- backend/functions/mods/r53-event-handler.js
+++ backend/functions/mods/r53-event-handler.js
@@ -1,17 +1,20 @@
-const ssm = require('./ssm-param-store')
+const ssm = require('./ssm-param-store');
+
+const ssm_key_prefix = `/brainbloomapps/${process.env.lifecycle}`;
 
 const handleCreateHostedZone = async (event) => {
-  const hostedZone = event.detail.responseElements.hostedZone
-  const key = "/HostedZoneConfig/Dev/id"
-  const index = hostedZone.id.lastIndexOf("/")
-  const value = hostedZone.id.substring(index + 1)
-  ssm.storeParam(key, value, "repbloom")
-}
+  const hostedZone = event.detail.responseElements.hostedZone;
+  const key = ssm_key_prefix + '/HostedZoneConfig/id';
+  const index = hostedZone.id.lastIndexOf('/');
+  const value = hostedZone.id.substring(index + 1);
+  ssm.storeParam(key, value, 'brainbloomapps');
+};
 
 exports.handleEvent = async (event) => {
-  if (event.detail.eventSource === 'route53.amazonaws.com'  &&
-      event.detail.eventName === 'CreateHostedZone') {
-        handleCreateHostedZone(event);
+  if (
+    event.detail.eventSource === 'route53.amazonaws.com' &&
+    event.detail.eventName === 'CreateHostedZone'
+  ) {
+    handleCreateHostedZone(event);
   }
-
-}
+};
